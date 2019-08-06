8e2b0de13106e12c606a9930000992d9b6cd501c
Wasswa Samuel <latimerscope@gmail.com>
2019-08-06 22:17:19 +0200

    first commit

---
+++ days_of_power.py
@@ -0,0 +1,7 @@
+def get_days_power(R1,D1,R2,D2,R3,D3,K):
+    print("Hello {R1} {D1} {R2} {D2} {R3} {D3} {K}".format(R1=R1,D1=D1,R2=R2,D2=D2,R3=R3,D3=D3,K=K))
+
+    #Use dictionary to track the days
+
+
+get_days_power(R1=1000, D1=3, R2=500, D2=10, R3=1500, D3=7, K=21000)
